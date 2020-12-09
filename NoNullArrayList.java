cf4ab42e7dbc50c06a6fe84411979d3b888be7c2
Christopher Liu <christopherliu@sorcero.com>
2020-12-09 14:15:34 -0500

    Add add methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -10,8 +10,22 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 
     public T set(int index, T element) {
         if (element == null) {
-            throw new IllegalArgumentException("Cannot add null to NoNullArrayList");
+            throw new IllegalArgumentException("Cannot set null to NoNullArrayList");
         }
         return super.set(index, element);
+    }
+
+    public void add(int index, T element) {
+        if (element == null) {
+            throw new IllegalArgumentException("Cannot add null to NoNullArrayList");
+        }
+        super.add(index, element);
+    }
+
+    public boolean add(T element) {
+        if (element == null) {
+            throw new IllegalArgumentException("Cannot add null to NoNullArrayList");
+        }
+        return super.add(element);
     }
 }
