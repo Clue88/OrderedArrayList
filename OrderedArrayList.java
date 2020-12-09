142ad2c48e0bfc9084d4e85776fc4a4e4db9262f
Christopher Liu <christopherliu@sorcero.com>
2020-12-09 14:31:01 -0500

    Add OrderedArrayList and constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,9 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+    public OrderedArrayList() {
+        super();
+    }
+
+    public OrderedArrayList(int initialCapacity) {
+        super(initialCapacity);
+    }
+}
